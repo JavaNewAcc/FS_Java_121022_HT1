4d0bab5508633cc3102020a5b5c0aa1346ecf363
Anton Braginets <java.new.acc@gmail.com>
2023-03-02 11:58:10 +0200

    Complete HT1

---
+++ src/firstlesson/Main.java
@@ -0,0 +1,22 @@
+package firstlesson;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Please enter coffee price:");
+		int coffeCost = scan.nextInt();
+
+		System.out.println("Please enter number of cups:");
+		int cups = scan.nextInt();
+
+		int totalSum;
+
+		totalSum = coffeCost * cups;
+
+		System.out.println(totalSum + " UAH");
+	}
+}
